a4e16cc3c811f2137f2941682ae60b705ff93f3c
valenzzaira <zairavalenz@gmail.com>
2025-10-07 18:53:19 -0600

    Estructura CiCd

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from pages.home_page import HomePage
+import os
 
 
 @pytest.fixture
@@ -8,8 +9,13 @@ def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
     options.add_argument("--incognito")
+    if os.getenv("CI"):
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
+
 
     driver.implicitly_wait(5)
     yield driver
@@ -19,6 +25,9 @@ def driver():
 def cinema_driver(driver):
     """Fixture que siempre va a la página del cinema"""
     driver.get("https://fake-cinema.vercel.app/")
+
     return driver
+
+
 
 
